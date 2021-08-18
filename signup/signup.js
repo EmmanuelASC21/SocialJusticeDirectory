648a8a72f08ad6df15f6dda991d572af9fc1340d
unknown <mannyprep2016@gmail.com>
2021-08-18 17:58:11 -0400

    second commit

--- signup/signup.js
+++ signup/signup.js
@@ -1,23 +1,64 @@
+function setFormMessage(formElement, type, message){
+    const messageElement= formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success","form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message){
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement){
+    inputElement.classList.remove("form__input--error"); 
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#Login")
     const createForm = document.querySelector("#CreateAccount")
     const forgotForm = document.querySelector("#forgotacc")
 
-    document.querySelector("#linkCreateAccount").addEventListener("click",() => {
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
         loginForm.classList.add("form--hidden");
         createForm.classList.remove("form--hidden");
         forgotForm.classList.add("form--hidden");
     });
     
-    document.querySelector("#linkLogin").addEventListener("click",() => {
+    document.querySelector("#linkLogin").addEventListener("click",e => {
+        e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createForm.classList.add("form--hidden");
         forgotForm.classList.add("form--hidden");
     });
 
-    document.querySelector("#linkforgot").addEventListener("click",() => {
+    document.querySelector("#linkforgot").addEventListener("click",e => {
+        e.preventDefault();
         forgotForm.classList.remove("form--hidden");
         createForm.classList.add("form--hidden");
         loginForm.classList.add("form--hidden");
+    });
+
+    // loginForm.addEventListener("submit", e => {
+    //     e.preventDefault();
+
+    //     //perform AJAX/Fetch login
+
+    //     setFormMessage(loginForm, "error", "invalid username or password")
+    // });
+
+    document.querySelectorAll(".form__input").forEach(inputElement =>{
+        inputElement.addEventListener("blur", e =>{
+            if(e.target.id == "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10){
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
     });
 });
