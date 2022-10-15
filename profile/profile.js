506f06be5a573d1a0fb9de1f82afa28fd840d3c5
Glitch (cvlcntr) <none>
2022-10-15 03:17:56 +0000

    🎈🧇 Updated with Glitch

---
+++ profile/profile.js
@@ -0,0 +1,12 @@
+
+    <ul id="profileNav">
+      <li>
+        <a href="#"><img class="pfp" src="/images/E.png"></a>
+        <ul>
+          <a href="profile.html" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Profile</a>
+          <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Settings</a>
+          <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Notifications</a>
+          <a href="index.html" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Log Out</a>
+        </ul>
+      </li>
+    </ul>
